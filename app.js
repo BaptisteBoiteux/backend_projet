e9ff77c5366b6a842bee13cbc9fb8dc97a86e12c
Baptiste <baptiste.boiteux@cpe.fr>
2022-05-17 14:45:03 +0200

    commit de fin

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static('C:\Users\bapti\Documents\CPE\S8\Projet\ProjetTransversal\WEB_ANGULAR\Larry\dist\Larry'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
